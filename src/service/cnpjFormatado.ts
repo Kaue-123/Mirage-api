cb26a85b76c3278ed45c2cdabc2fa350f7d3d500
Kaue Carmo <kauecarmo111@gmail.com>
2025-03-27 11:05:32 -0300

    ....

--- src/service/cnpjFormatado.ts
+++
@@ -1,7 +0,0 @@
-export function formatarCnpj(cnpj) {
-    return cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
-}
-
-export function limparCNPJ(cnpj: string): string {
-    return cnpj.replace(/[^\d]/g, ''); // Remove tudo que não é número
-}
