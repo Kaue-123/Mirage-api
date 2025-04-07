164d6fc39ee7b777aa3481eca54496ca4cffa7ce
Kauê <kauecarmo111@gmail.com>
2025-04-07 17:04:11 -0300

    Testando job para baixar mensagens

--- src/jobs/job.switchProfile.ts
+++ src/jobs/job.switchProfile.ts
@@ -1,170 +1,61 @@
-// import puppeteer from 'puppeteer-extra';
-// import { Page } from 'puppeteer';
-// import axios from 'axios';
+import { Page } from 'puppeteer';
+import { AppdataSource } from '../db/data-source';
+import { Enterprise } from '../entities/Enterprises';
 
-// export class PuppeteerService {
-//     private certificadoCnpj = '34331182000103'; 
-//     private bearerToken: string;
-//     private apiUrl = process.env.BASE_URL;
+export class MessageDownloader {
+    private page: Page;
 
-//     constructor() {
-//         this.bearerToken = '';
-//     }
+    constructor(page: Page) {
+        this.page = page;
+    }
 
-//     async loginWithCertificate(): Promise<{ page: Page; bearerToken: string }> {
-      
-//         const browser = await puppeteer.launch({
-//             headless: false,
-//             executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-//             userDataDir: 'C:\\Users\\Kaue\\AppData\\Local\\Google\\Chrome\\User Data\\Default',
-//             args: [
-//                 '--start-maximized',
-//                 '--force-device-scale-factor=0.9',
-//                 '--ignore-certificate-errors',
-//                 '--allow-running-insecure-content',
-//                 '--auto-select-certificate-for-urls={"pattern":"https://det.sit.trabalho.gov.br","filter":{}}',
-//                 '--ssl-client-cert-file=C:\\Users\\Kaue\\Documentos\\Certificados\\cert.pem',
-//                 '--ssl-client-key-file=C:\\Users\\Kaue\\Documentos\\Certificados\\key.pem',
-//                 '--ssl-client-key-passphrase=27713412808'
-//             ],
-//             defaultViewport: { width: 1360, height: 768 },
-//         });
+    async acessarMensagensNaoLidas(cnpj: string): Promise<void> {
+        const enterpriseRepository = AppdataSource.getRepository(Enterprise)
 
-//         const page: Page = await browser.newPage();
-//         await page.setViewport({ width: 1360, height: 768 });
-//         await page.goto('https://det.sit.trabalho.gov.br/login?r=%2Fservicos%2Fdet%2Findex.html');
+        if (enterpriseRepository) {
+            console.error("Erro ao obter repositório de empresas.");
+            return;
+        }
 
-//         let bearerToken = '';
+        await this.page.waitForSelector('.is-secondary.slim_button.br-button', { visible: true });
+        await this.page.click('.is-secondary.slim_button.br-button');
+        console.log("Botão 'Trocar Perfil' clicado.");
 
-//         // Iniciar login
-//         await page.waitForSelector('#botao');
-//         await page.click('#botao');
-//         await page.waitForSelector('#login-certificate', { visible: true, timeout: 40000 });
-//         await page.click('#login-certificate');
 
-//         console.log("Enviando hCaptcha para o 2Captcha...");
-//         const { data } = await axios.post('http://2captcha.com/in.php', null, {
-//             params: {
-//                 key: "c60b640dd62d387ef1c4bcb792add8c6",
-//                 method: 'hcaptcha',
-//                 sitekey: '93b08d40-d46c-400a-ba07-6f91cda815b9',
-//                 pageurl: 'https://det.sit.trabalho.gov.br/login?r=%2Fservicos%2Fdet%2Findex.html',
-//                 json: 1
-//             }
-//         });
+        await this.page.waitForNavigation({ waitUntil: 'networkidle2' });
 
-//         if (data.status !== 1) {
-//             console.error("Erro ao enviar hCaptcha para 2Captcha", data);
-//             throw new Error("Falha no hCaptcha");
-//         }
 
-//         console.log("Captcha enviado. Task ID:", data.request);
-//         const taskId = data.request;
+        await this.page.waitForSelector('.ng-arrow-wrapper', { visible: true })
+        await this.page.click('.ng-arrow-wrapper')
+        console.log("Botão de trocar tipo de perfil selecionado")
 
-//         try {
-//             let captchaToken;
-//             for (let i = 0; i < 30; i++) {
-//                 await new Promise(resolve => setTimeout(resolve, 500));
-//                 const { data: solutionResponse } = await axios.get('http://2captcha.com/res.php', {
-//                     params: {
-//                         key: "c60b640dd62d387ef1c4bcb792add8c6",
-//                         action: 'get',
-//                         id: taskId,
-//                         json: 1
-//                     }
-//                 });
-    
-//                 if (solutionResponse.status === 1) {
-//                     captchaToken = solutionResponse.request;
-//                     break;
-//                 }
-//             }
-    
-//             if (!captchaToken) {
-//                 console.error("Token do captcha não encontrado");
-//                 throw new Error("Falha ao resolver captcha");
-//             }
-    
-    
-//             await new Promise(resolve => setTimeout(resolve, 1000));
-//             await page.evaluate((captchaToken) => {
-//                 const captchaInput = document.querySelector('[name="h-captcha-response"]') as HTMLInputElement;
-//                 if (captchaInput) {
-//                     captchaInput.value = captchaToken;
-//                     captchaInput.dispatchEvent(new Event('input', { bubbles: true }));
-//                     captchaInput.dispatchEvent(new Event('change', { bubbles: true }));
-//                     console.log("Token do captcha injetado.");
-//                 } else {
-//                     console.error("Input do captcha não encontrado");
-//                 }
-//             }, captchaToken);
-//         } catch (error) {
-//             console.error("Erro ao resolver o captcha", error.message)
-//         }
+
+        await this.page.waitForSelector('.ng-dropdown-panel .ng-option', { visible: true });
+        const options = await this.page.$$('.ng-dropdown-panel .ng-option');
+        for (const option of options) {
+            const text = await this.page.evaluate(el => el.textContent, option);
+            if (text?.trim() === "Procurador") {
+                await option.click();
+                console.log("Perfil 'Procurador' selecionado.");
+                break;
+            }
+        }
+
+
+        await this.page.waitForSelector("input[placeholder='Informe CNPJ ou CPF']", { visible: true });
+        await this.page.click("input[placeholder='Informe CNPJ ou CPF']");
+        await this.page.evaluate(() => document.querySelector("input[placeholder='Informe CNPJ ou CPF']")?.setAttribute("value", ""));
+        await this.page.type("input[placeholder='Informe CNPJ ou CPF']", cnpj, { delay: 30 });
+
+        await this.page.waitForSelector('br-button is-primary', { visible: true })
+        await this.page.click('br-button is-primary')
+        console.log("Botão para selecionar perfil clicado.")
+
+        await this.page.waitForNavigation({ waitUntil: 'networkidle2' });
+
 
         
-//         // Esperar o botão de trocar perfil aparecer e clicar
-//         await page.waitForSelector('.is-secondary.slim_button.br-button', { visible: true });
-//         await page.click('.is-secondary.slim_button.br-button');
-//         console.log("Botão 'Trocar Perfil' clicado.");
-
-//         // Opcional: aguardar a página carregar após a troca de perfil
-//         await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
-
-//         await page.waitForSelector('ng-arrow-wrapper', { visible: true })
-//         await page.click('ng-arrow-wrapper')
-//         console.log("Botão de trocar tipo de perfil selecionado")
-
-
-//         await page.waitForSelector('option[placeholder="Procurador"]', { visible: true });
-//         await page.select('select', 'Procurador');
-//         console.log("Opção 'Procurador' selecionada.");
-
-
-//         await page.waitForSelector('br-button is-primary', { visible: true })
-//         await page.click('br-button is-primary')
-//         console.log("Botão para selecionar perfil clicado.")
-
-//         await page.waitForNavigation({ waitUntil: 'networkidle2' });
-
         
-       
+        }
+    }
 
-      
-//         await page.evaluate(() => {
-//             const form = document.querySelector("form");
-//             if (form) {
-//                 form.submit();
-//                 console.log("Formulário enviado.");
-//             }
-//         });
-
-//         await new Promise((resolve, reject) => {
-
-//             page.on('response', async (response) => {
-//                 const url = response.url();
-//                 const status = response.status();
-
-//                 if (status === 200 && url.includes(`https://det.sit.trabalho.gov.br/services/v1/empregadores/${this.certificadoCnpj}/cadastrado`)) {
-//                     const tokenHeader = response.headers()['set-token'];
-//                     if (tokenHeader) {
-//                         this.bearerToken = tokenHeader;
-//                         console.log("Bearer Token capturado da resposta:", this.bearerToken);
-//                         resolve(void 0);
-//                     }
-//                 }
-//             });
-//             setTimeout(() => {
-//                 reject(new Error("Bearer Token não capturado após login"));
-//             }, 10000); 
-//         });
-//         if (!this.bearerToken) {
-//             throw new Error("Bearer Token não capturado após login");
-//         }
-//         return { page, bearerToken: this.bearerToken };
-//     }
-//     getBearerToken(): string { 
-//         return this.bearerToken;
-//     }
-// }
